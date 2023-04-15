54a5b4ab60fdd62190946e8d9ff44d6be146b88e
Your Name <you@example.com>
2023-04-15 17:33:08 -0400

    _

--- webui.bat
+++ webui.bat
@@ -50,7 +50,7 @@ set ACCELERATE="%VENV_DIR%\Scripts\accelerate.exe"
 if EXIST %ACCELERATE% goto :accelerate_launch
 
 :launch
-%PYTHON% launch.py %*
+%PYTHON% launch.py --listen --enable-insecure-extension-access --api --disable-nan-check --xformers %*
 pause
 exit /b
 
